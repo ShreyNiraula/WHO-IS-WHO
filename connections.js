5e2f0bed886d532f3e7a4199f3f8028016533b63
Shrey Niraula <nshrey53@gmail.com>
2020-12-02 08:12:07 +0545

    minor db setting

--- connections.js
+++ connections.js
@@ -1,18 +1,12 @@
 var mysql = require("mysql");
-// require("dotenv").config();
+// DOT ENV
+const dotenv = require("dotenv");
+dotenv.config();
 
-// const db = mysql.createConnection({
-//   host: "bj5wulbhjpvrt6obt3me-mysql.services.clever-cloud.com",
-//   user: "un0gdi6442coj4mb",
-//   database: "bj5wulbhjpvrt6obt3me",
-//   password: "OypporrOgolz6s9XYS08",
-//   port: 3306,
-// });
+const DATABASE_CREDENTIAL = process.env.DATABASE_CREDENTIAL;
 
 // TODO: later change to dot evn
-const db = mysql.createConnection(
-  "mysql://un0gdi6442coj4mb:OypporrOgolz6s9XYS08@bj5wulbhjpvrt6obt3me-mysql.services.clever-cloud.com:3306/bj5wulbhjpvrt6obt3me"
-);
+const db = mysql.createConnection(DATABASE_CREDENTIAL);
 
 // db connection
 db.connect((err) => {
