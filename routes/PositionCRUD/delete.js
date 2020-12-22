97f11a27040f2ffe0b456ecb273d6bc61a3f789e
Shrey Niraula <nshrey53@gmail.com>
2020-12-23 00:11:21 +0545

    query string of cid for delete operation

--- routes/PositionCRUD/delete.js
+++ routes/PositionCRUD/delete.js
@@ -7,18 +7,18 @@ var db = require("../../connections");
 // delete cause cascade delete to position table as well.
 router.get("/:id/:pid", (req, res, next) => {
     const { id, pid } = req.params;
+    const {cid} = req.query;
     let sql = `DELETE c,p FROM Candidate as c 
     NATURAL JOIN Position as p 
     NATURAL JOIN Designation as d 
     WHERE p.Ministry_id = '${id}' AND p.Position='${pid}';
     `;
     db.query(sql, (err, result) => {
+      console.log('the deleted result is:', result)
       if (err) throw err;
       res.redirect(`/minister/${id}`); // redirect
     });
   });
-
-
-  module.exports = router;
+module.exports = router;
 
   
