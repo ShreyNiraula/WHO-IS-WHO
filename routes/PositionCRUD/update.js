d2f7b63ec7eec0fb28d965b63401e276e2b99dce
Shrey Niraula <nshrey53@gmail.com>
2020-12-22 21:00:08 +0545

    position in candidate table missing

--- routes/PositionCRUD/update.js
+++ routes/PositionCRUD/update.js
@@ -38,7 +38,7 @@ router.post("/:id/:pid/:cid", urlEncodedParser, (req, res, next) => {
     } = req.body;
 
     // update both candidate table that causes update cascade to position table as well.
-    let sql = `UPDATE Candidate as c NATURAL JOIN Position as p NATURAL JOIN Designation as d SET c.Candidate_id=${cid}, c.Initials='${Initials}', c.First_Name='${First_Name}', c.Middle_Name='${Middle_Name}', c.Last_Name='${Last_Name}', c.Photo_URL='${Photo_URL}', p.Ministry_id='${id}', p.Position='${pid}', p.Department ='${Department}'  WHERE p.Ministry_id = '${id}' AND p.Position='${pid}'`;
+    let sql = `UPDATE Candidate as c NATURAL JOIN Position as p SET c.Candidate_id=${cid}, c.Initials='${Initials}', c.First_Name='${First_Name}', c.Middle_Name='${Middle_Name}', c.Last_Name='${Last_Name}', c.Photo_URL='${Photo_URL}', p.Ministry_id='${id}', p.Position='${pid}', p.Department ='${Department}'  WHERE p.Ministry_id = '${id}' AND p.Position='${pid}'`;
     db.query(sql, (err, result) => {
       if (err) throw err;
       res.redirect(`/minister/${id}`); 
