9c6e902a8528089262011f7df773e16304993c88
Shrey Niraula <nshrey53@gmail.com>
2020-12-23 09:46:13 +0545

    history moved within card

--- routes/PositionCRUD/delete.js
+++ routes/PositionCRUD/delete.js
@@ -8,16 +8,17 @@ var db = require("../../connections");
 router.get("/:id/:pid", (req, res, next) => {
     const { id, pid } = req.params;
     const {cid} = req.query;
-    let sql = `DELETE c,p FROM Candidate as c 
-    NATURAL JOIN Position as p 
-    NATURAL JOIN Designation as d 
-    WHERE p.Ministry_id = '${id}' AND p.Position='${pid}';
-    `;
-    db.query(sql, (err, result) => {
-      console.log('the deleted result is:', result) // useless info
-      if (err) throw err;
-      res.redirect(`/minister/${id}`); // redirect
-    });
+      // go for simple delete
+      let sql = `DELETE c,p FROM Candidate as c 
+      NATURAL JOIN Position as p 
+      NATURAL JOIN Designation as d 
+      WHERE p.Ministry_id = '${id}' AND p.Position='${pid}';
+      `;
+      db.query(sql, (err, result) => {
+        console.log('the deleted result is:', result) // useless info
+        if (err) throw err;
+        res.redirect(`/minister/${id}`); // redirect
+      });
   });
 module.exports = router;
 
